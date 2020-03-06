057210db79aad4f2bed0b638c67382b8b9d1d5e8
yorps <7090963+yorps@users.noreply.github.com>
2020-03-06 22:01:05 +0100

    removed unused constructor

--- src/components/AlbumIcon.js
+++ src/components/AlbumIcon.js
@@ -2,18 +2,6 @@ import React, { Component } from "react";
 
 class AlbumIcon extends Component {
 
-    constructor(props) {
-        super(props);
-
-        /*
-        this.state = {
-            id: this.props.id,
-            name: this.props.name,
-            imgUrl: this.props.imgUrl
-        };*/
-    }
-
-
     render() {
         return <div className="albumIcon" 
         name={this.props.name} 
