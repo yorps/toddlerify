82cee7bc262040e6da219b6c2337879851f15676
yorps <7090963+yorps@users.noreply.github.com>
2020-03-06 21:34:48 +0100

    add routing

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AlbumsByArtistList from './components/AlbumsByArtistList'
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const routing = (
+
+    <Router>
+            <Route exact path="/" component={App} />
+            <Route path="/albums/:artistId/:accessToken" component={AlbumsByArtistList} />
+    </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
