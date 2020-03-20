57f6229f54c60062823a836e56b312ddd927ac39
yorps <7090963+yorps@users.noreply.github.com>
2020-03-20 17:51:48 +0100

    edit redirect page

--- src/config_example.js
+++ src/config_example.js
@@ -1,7 +1,7 @@
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "YOUR_CLIENT_ID_HERE";
 export const clientSecret = "YOUR_CLIENT_SECRET_HERE";
-export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "http://localhost:3000/";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
